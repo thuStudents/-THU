d2e6e089f57966243dbb92e20cafe40c86a926d2
Asian Hossain <aasianhossain@gmail.com>
2021-11-01 10:01:21 +0100

    firebase realtime database connected

--- app/src/main/java/de/thu/MainActivity.java
+++ app/src/main/java/de/thu/MainActivity.java
@@ -14,46 +14,10 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText postText;
-    Button postButton;
-    DatabaseReference ref;
-    Post post;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Toast.makeText(MainActivity.this, "Firebase Connection Success",Toast.LENGTH_LONG).show();
-
-        postText = (EditText) findViewById(R.id.postText);
-        postButton = (Button) findViewById(R.id.postButton);
-
-        post = new Post();
-        ref = FirebaseDatabase.getInstance().getReference().child("Post");
-
-        postButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                post.setPostText(postText.getText().toString().trim());
-
-
-
-
-                ref.push().setValue(post);
-
-                Toast.makeText(MainActivity.this, "Post inserted",Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-
-
-
-
-
-
-
+        Toast.makeText(MainActivity.this, "Firebase Connection Success",Toast.LENGTH_LONG).show();
     }
 }
