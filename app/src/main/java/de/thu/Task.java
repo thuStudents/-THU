b9de5128f1bafa79594305291df04eb98b31297a
Asian Hossain <aasianhossain@gmail.com>
2022-01-13 14:03:29 +0100

    timetable task and todo task completed

---
+++ app/src/main/java/de/thu/Task.java
@@ -0,0 +1,17 @@
+package de.thu;
+
+public class Task {
+
+    private String task;
+
+    public Task() {
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
